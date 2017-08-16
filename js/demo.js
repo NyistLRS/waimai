ce33f9c3655260565fda6aab0c942212cc5d7e4e
lrs <lrs@192.168.1.102>
2017-08-16 22:19:32 +0800

    组件和路由

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,8 @@
-var router = new VueRouter({ // 路由要挂载在vue实例上
-			mdoe: history
+		var router = new VueRouter({ // 路由要挂载在vue实例上
+			mdoe: history,
+			routes:[
+				{path:'/',component:temp1}
+			]
 		});
 		new Vue({
 			el: "#app",
@@ -9,19 +12,9 @@ var router = new VueRouter({ // 路由要挂载在vue实例上
 				selected:"index"
 			},
 			methods:{
-			},
-			watch:{
-				selected : function(val){
-					if(val == 'index'){
-						return this.active = 'tab-container1';
-					}
-					if(val == 'car'){
-						return this.active = 'tab-container2';
-					}
-					if(val == 'me'){
-						return this.active = 'tab-container3';
-					}
-				}
+				servicesChange (val) {
+                	this.selected = val;
+            	}
 			},
 			router
 		})
